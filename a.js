b07ca2a00e1283c5387d3d31b7be4a5137cdd558
Salim Cabus <83632722+salimcabus@users.noreply.github.com>
2024-05-03 23:24:06 -0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,3 @@
+document.querySelector('body').insertAdjacentHTML('afterbegin', '<iframe src="https://98.js.org/" id="windows98"></iframe>');
+document.querySelector('#windows98').style.width = '100%';
+document.querySelector('#windows98').style.height = '100vh';
