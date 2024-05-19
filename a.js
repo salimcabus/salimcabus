75ea5abef16c06a56dde804ee9f1173a77203f07
Salim Cabus <83632722+salimcabus@users.noreply.github.com>
2024-05-18 23:16:04 -0300

    Delete a.js

--- a.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('body').insertAdjacentHTML('afterbegin', '<iframe src="https://98.js.org/" id="windows98"></iframe>');
-document.querySelector('#windows98').style.width = '100%';
-document.querySelector('#windows98').style.height = '100vh';
